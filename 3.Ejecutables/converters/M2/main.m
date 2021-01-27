22d0a615a06d15b5c1c40b77d1f84ff249ab58de
jacquesfauquex <jacquesfauquex@gmail.com>
2021-01-27 09:10:22 -0300

    M2 secuential xslt

--- 3.Ejecutables/converters/M2/main.m
+++ 3.Ejecutables/converters/M2/main.m
@@ -7,7 +7,7 @@
 //special cases include :
 // xslt1 B.xslt (further serializes in bson)
 // xslt1 D.xslt (further serializes in binary dicom)
-//wherehttps://github.com/jacquesfauquex/DICOM_contextualizedKey-values/blob/master/xml/xmldicom.xsd
+
 
 
 int main(int argc, const char * argv[]) {
@@ -19,99 +19,88 @@ int main(int argc, const char * argv[]) {
 
       NSDictionary *environment=processInfo.environment;
       
-      //X2XlogLevel
-      if (environment[@"X2XlogLevel"])
+      //M2logLevel
+      if (environment[@"M2logLevel"])
       {
-         NSUInteger logLevel=[@[@"DEBUG",@"VERBOSE",@"INFO",@"WARNING",@"ERROR",@"EXCEPTION"] indexOfObject:environment[@"X2XlogLevel"]];
+         NSUInteger logLevel=[@[@"DEBUG",@"VERBOSE",@"INFO",@"WARNING",@"ERROR",@"EXCEPTION"] indexOfObject:environment[@"M2logLevel"]];
          if (logLevel!=NSNotFound) ODLogLevel=(ODLogLevelEnum)logLevel;
          else ODLogLevel=4;//ERROR (default)
       }
       else ODLogLevel=4;//ERROR (default)
       
       
-      //X2XlogPath
-      NSString *logPath=environment[@"X2XlogPath"];
+      //M2logPath
+      NSString *logPath=environment[@"M2logPath"];
       if (logPath && ([logPath hasPrefix:@"/Users/Shared"] || [logPath hasPrefix:@"/Volumes/LOG"]))
       {
          if ([logPath hasSuffix:@".log"])
             freopen([logPath cStringUsingEncoding:NSASCIIStringEncoding], "a+", stderr);
          else freopen([[logPath stringByAppendingPathExtension:@".log"] cStringUsingEncoding:NSASCIIStringEncoding], "a+", stderr);
       }
-      else freopen([@"/Users/Shared/X2X.log" cStringUsingEncoding:NSASCIIStringEncoding], "a+", stderr);
+      else freopen([@"/Users/Shared/M2.log" cStringUsingEncoding:NSASCIIStringEncoding], "a+", stderr);
       
       //M2output
       NSString *M2output=environment[@"M2output"];
       if (!M2output) M2output=@"/dev/stdout";
 
-      //X2XtestPath
-      NSData *linedata=nil;
-      NSString *testPath=environment[@"X2XtestPath"];
-      if (testPath) linedata=[NSData dataWithContentsOfFile:testPath];
-      else linedata = [[NSFileHandle fileHandleWithStandardInput] availableData];
-      
+      //M2testPath
+      NSData *data=nil;
+      NSString *testPath=environment[@"M2testPath"];
+      if (testPath) data=[NSData dataWithContentsOfFile:testPath];
+      else data = [[NSFileHandle fileHandleWithStandardInput] availableData];
       
       LOG_DEBUG(@"%@",[environment description]);
       
-      
-#pragma marks args
-      
-      NSArray *args=[[NSProcessInfo processInfo] arguments];
-      
-      if (args.count==1) [linedata writeToFile:M2output atomically:NO]; //without args: in>out
-      else //X2X [XSL1TransformationPath [params...]]
+      id result=data;
+      if (data.length > 134)
       {
-         NSData *xsl1data=[NSData dataWithContentsOfFile:args[1]];
-         if (!xsl1data)
-         {
-            LOG_ERROR(@"arg XSL1TransformationPath %@ not available",args[1]);
-            exit(2);
-         }
-
-         NSMutableDictionary *xslparams=[NSMutableDictionary dictionary];
-         for (NSString *string in [args subarrayWithRange:NSMakeRange(2,args.count - 2)])
-         {
-            NSArray *keyValue=[string componentsSeparatedByString:@"="];
-            if (keyValue.count != 2)
-            {
-               LOG_ERROR(@"xsl1t params in %@ should be key=value",args[1]);
-               exit(3);
-            }
-            [xslparams setValue:keyValue[1] forKey:keyValue[0]];
-         }
-
-         LOG_DEBUG(@"xsl1t %@ with params : %@",args[1],[xslparams description]);
+         //minimal xml:
+         /* <map xmlns="http://www.w3.org/2005/xpath-functions"><map key="dataset"><array key="00000001-00020000_UL"></number></array></map></map>*/
+#pragma marks args
          
-   #pragma mark in out
-         if (linedata.length)
+         NSArray *args=processInfo.arguments;
+         NSUInteger argscount=args.count;
+         NSArray *xslt1Paths=nil;
+         NSXMLDocument *xmlDocument=nil;
+         NSError *error=nil;
+         if (argscount>1)
          {
+            xmlDocument=[[NSXMLDocument alloc] initWithData:data options:0 error:&error];
+            if (error)
+            {
+               NSLog(@"Error 1 reading M xml document");
+               exit(1);
+            }
+            xslt1Paths =[args subarrayWithRange:NSMakeRange(1, argscount-1)];
+         }
+         else xslt1Paths=[NSArray array];//empty array
+
+         for (NSString *xslt1Path in xslt1Paths)
+         {
+            NSData *xsl1data=[NSData dataWithContentsOfFile:xslt1Path];
+            if (!xsl1data)
+            {
+               LOG_ERROR(@"arg XSL1TransformationPath %@ not available",args[1]);
+               exit(2);
+            }
+
+            NSMutableDictionary *xslparams=environment[[xslt1Path lastPathComponent]];
+            LOG_DEBUG(@"xsl1t %@ with params : %@",args[1],[xslparams description]);
+            
             NSError *error=nil;
-            NSXMLDocument *xmlDocument=[[NSXMLDocument alloc]initWithData:linedata options:0 error:&error];
-            //https://developer.apple.com/documentation/foundation/nsxmlnodeoptions
-            if (!xmlDocument)
+            id result=[xmlDocument objectByApplyingXSLT:xsl1data arguments:xslparams error:&error];
+            if (!result)
             {
-               if (error) LOG_INFO(@"%@",[error description]);
-               [[NSData data] writeToFile:M2output atomically:NO];
+               LOG_WARNING(@"Error 5 with xsl %@",[args description]);
+               [[NSData data] writeToFile:@"/dev/stdout" atomically:NO];
+               exit(5);
             }
-            else
-            {
-               id result=[xmlDocument objectByApplyingXSLT:xsl1data arguments:xslparams error:&error];
-               if (!result)
-               {
-                  LOG_WARNING(@"Error with xsl %@",[args description]);
-               }
-               else if ([result isMemberOfClass:[NSXMLDocument class]])
-               {
-                  LOG_VERBOSE(@"xml result");
-                  [[result XMLData] writeToFile:M2output atomically:NO];
-               }
-               else
-               {
-                  LOG_VERBOSE(@"data result");
-                  [result writeToFile:M2output atomically:NO];
-               }
-            }
+            [xmlDocument setRootElement:[result rootElement]];
          }
       }
+      if ([result isMemberOfClass:[NSXMLDocument class]])
+         [[result XMLData] writeToFile:M2output atomically:NO];
+      else [result writeToFile:M2output atomically:NO];
    }//end autorelease pool
    return 0;
 }
