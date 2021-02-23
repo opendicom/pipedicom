a970bbdee431e1442ffabb686a96450d9438e531
jacquesfauquex <jacquesfauquex@gmail.com>
2021-02-23 19:56:41 -0300

    D2xml D2dict

---
+++ 3.Ejecutables/converters/D2J/D2dict.h
@@ -0,0 +1,13 @@
+//
+//  dict4D.h
+//  converters
+//
+//  Created by jacquesfauquex on 2021-02-23.
+//
+
+#ifndef D2dict_h
+#define D2dict_h
+
+int D2dict(NSData *data, NSMutableDictionary *dict);
+
+#endif /* D2dict_h */
---
+++ 3.Ejecutables/converters/D2M/D2xml.h
@@ -0,0 +1,12 @@
+//
+//  D2xml.h
+//  converters
+//
+//  Created by jacquesfauquex on 2021-02-23.
+//
+
+#ifndef D2xml_h
+#define D2xml_h
+
+int D2xml(NSData *data, NSXMLElement *xml);
+#endif /* D2xml_h */
