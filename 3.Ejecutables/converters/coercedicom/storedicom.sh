4d398513aed64542b2d1c043aebb30a5f3e62edb
jacquesfauquex <jacquesfauquex@gmail.com>
2021-07-28 13:08:54 -0300

    Update storedicom.sh

--- 3.Ejecutables/converters/coercedicom/storedicom.sh
+++ 3.Ejecutables/converters/coercedicom/storedicom.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+   #!/bin/sh
 SECONDS=0
 PATH=$PATH:/usr/local/bin:/usr/bin
 
@@ -10,81 +10,93 @@ PATH=$PATH:/usr/local/bin:/usr/bin
 # 4 WARNING
 # 5 STORE URL (with %@ for PACS ORG AET found at first level of SEND
 #'https://serviciosridi.preprod.asse.uy/dcm4chee-arc/stow/'"DCM4CHEE"'/studies'
-# 6 WRITE_RESP
-# 7 CURL_VERBOSE
-
-
-# LOOPS: ORG < MOD < E < BUCKET
+# 6 CURL_VERBOSE
 
 cd $1
-ORGS=$(ls)
-for ORG in "${ORGS[@]}"; do
-    cd $ORG
-    
-    if [[ $5 == *"+"* ]]; then
-        URLPREFIX="${5%+*}"
-        URLSUFFIX="${5#*+}"
-        URL="$URLPREFIX""$ORG""$URLSUFFIX" 
-    else
-        URL=$5
-    fi
-    
-    MODS=$(ls)
-    for MOD in "${MODS[@]}"; do
-        cd $MOD
-        
-        STUDIES=$(ls)
-        for STUDY in "${STUDIES[@]}"; do
-            cd $STUDY
-    
-            BUCKETS=$(ls)
-            for BUCKET in "${BUCKETS[@]}"; do
-                cd $BUCKET
+if [ ! -z "$(ls -A)" ]; then
+   for ORG in *; do
+   if [ -z "$(ls "$ORG")" ]; then
+          rm -Rf "$ORG"
+   else
+          cd $ORG
 
-                if [[ $* == *"CURL_VERBOSE"* ]]
-                then
-                    STORERESP=$( cat * | curl -v -X POST -H "Content-Type: multipart/related; type=\"application/dicom\"; boundary=myboundary" "$URL" --data-binary @- )
-                else
-                    STORERESP=$( cat * | curl -X POST -H "Content-Type: multipart/related; type=\"application/dicom\"; boundary=myboundary" "$URL" --data-binary @- )
-                fi
+          if [[ $5 == *"+"* ]]; then
+              URLPREFIX="${5%+*}"
+              URLSUFFIX="${5#*+}"
+              URL="$URLPREFIX""$ORG""$URLSUFFIX"
+          else
+              URL=$5
+          fi
 
-                if [[ -z $STORERESP ]]
-                then
-                    echo "$SECONDS" "$ORG" "$MOD" "$STUDY" "$BUCKET" 'no response'
-                else
-                
-                    #response
-                
-                    if [[ $STORERESP == *00081198* ]]
-                    then
-                        #FailedSOPSequence
-                        echo $STORERESP
-                         #http://dicom.nema.org/medical/dicom/current/output/chtml/part18/sect_6.6.html#table_6.6.1-4
-                        DSTBUCKETDIR="$3"/"$ORG"/"$MOD"/"$STUDY"/"$BUCKET"
-                         
-                    elif [[ $STORERESP == *00081199* ]]
-                    then
-                         #WarningSOPSequence
-                         echo $STORERESP
-                         DSTBUCKETDIR="$4"/"$ORG"/"$MOD"/"$STUDY"/"$BUCKET"
-                    else
-                        if [[ $* == *"WRITE_RESP"* ]] 
-                        then
-                            echo $STORERESP
-                        fi
-                        DSTBUCKETDIR="$2"/"$ORG"/"$MOD"/"$STUDY"/"$BUCKET"
-                    fi   
-                    [ ! -d "$DSTBUCKETDIR" ] && mkdir -p "$BUCKETDIR"                    
-                    mv "$BUCKET"/* "$BUCKETDIR"               
-                fi            
-            done
-            #BUCKET
-            cd ..
-        done
-        #STUDY
-        cd ..
-    done
-    #MOD
-    cd ..
-done
+          for SOURCE in *; do
+              if [ -z "$(ls "$SOURCE")" ]; then
+                 rm -Rf "$SOURCE"
+              else
+echo $SOURCE
+                 cd $SOURCE
+
+                 for STUDY in *; do
+                     if [ -z "$(ls "$STUDY")" ]; then
+                        rm -Rf "$STUDY"
+                     else
+                        cd $STUDY
+
+                        for BUCKET in *; do
+                           if [ -z "$(ls "$BUCKET")" ]; then
+                               rm -Rf "$BUCKET"
+                           else
+                               cd $BUCKET
+
+                               if [[ $* == *"CURL_VERBOSE"* ]]
+                               then
+                                   STORERESP=$( cat * | curl -X POST -H "Content-Type: multipart/related; type=\"application/dicom\"; boundary=myboundary" "$URL" --data-binary @- )
+                               else
+                                   STORERESP=$( cat * | curl -X POST -H "Content-Type: multipart/related; type=\"application/dicom\"; boundary=myboundary" "$URL" --data-binary @- )
+                               fi
+
+                               if [[ -z $STORERESP ]]
+                               then
+                                   echo "$ORG"/"$SOURCE"/"$STUDY"/"$BUCKET"/ 'no response'
+                                   cd ..
+                               else
+                                   #response
+
+                                   if [[ $STORERESP == *00081198* ]]
+                                   then
+                                       #FailedSOPSequence
+                                        #http://dicom.nema.org/medical/dicom/current/output/chtml/part18/sect_6.6.html#table_6.6.1-4
+                                       DSTBUCKETDIR="$3"/"$ORG"/"$SOURCE"/"$STUDY"/"$BUCKET"/
+
+                                   elif [[ $STORERESP == *00081199* ]]
+                                   then
+                                        #WarningSOPSequence
+                                     DSTBUCKETDIR="$4"/"$ORG"/"$SOURCE"/"$STUDY"/"$BUCKET"/
+                                   else
+                                       DSTBUCKETDIR="$2"/"$ORG"/"$SOURCE"/"$STUDY"/"$BUCKET"/
+                                   fi
+                                   if [ ! -d "$DSTBUCKETDIR" ]
+                                   then
+                                       mkdir -p "$DSTBUCKETDIR"
+                                   fi
+                                   mv * "$DSTBUCKETDIR"
+                                   echo $STORERESP > "$DSTBUCKETDIR"'.response'
+                                   cd ..
+                                   rm -Rf "$BUCKET"
+                               fi
+                               #RESPONSE
+                            fi
+                        done
+                        #BUCKET
+                        cd ..
+                     fi
+                 done
+                 #STUDY
+                 cd ..
+               fi
+          done
+          #SOURCE
+          cd ..
+      fi
+   done
+fi
 #ORG
