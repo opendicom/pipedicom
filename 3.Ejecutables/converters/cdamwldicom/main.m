2bf79b868cdd77eb51b7a230cbf446b0fbd566bd
jacquesfauquex <jacquesfauquex@gmail.com>
2022-09-07 10:48:36 -0300

    use wado-uri  and remove dependencia a zipzap

--- 3.Ejecutables/converters/cdamwldicom/main.m
+++ 3.Ejecutables/converters/cdamwldicom/main.m
@@ -2,11 +2,6 @@
 //  Copyright (c) 2014-2021 opendicom.com. All rights reserved.
 
 #import <Foundation/Foundation.h>
-#import "ZZArchiveEntry.h"
-#import "ZZArchive.h"
-#import "ZZConstants.h"
-#import "ZZChannel.h"
-#import "ZZError.h"
 
 #import <DCKV/dict2D.h>
 
@@ -26,7 +21,7 @@ int main(int argc, const char * argv[])
     const UInt64 _0002001_tag_vr=0x0000424F00010002;
     const UInt32 _0002001_length=0x00000002;
     const UInt16 _0002001_value=0x0001;
-    const uint64 tag00420011=0x0000424F00110042;//encapsulatedCDA tag + vr + padding (used in order to find the offset)
+ 
     NSFileManager *fileManager=[NSFileManager defaultManager];
 
     //http://unicode.org/reports/tr35/tr35-6.html#Date_Format_Patterns
@@ -44,8 +39,6 @@ int main(int argc, const char * argv[])
      //[2] qido base url
      //[3] audit folder
      //[4] scpaet
-
-     
      
      //[1]cda2mwl.xsl path
      NSData *xslt1Data=[NSData dataWithContentsOfFile:[args[1] stringByExpandingTildeInPath]];
@@ -167,125 +160,125 @@ int main(int argc, const char * argv[])
 
 #pragma mark download details
          NSString *RetrieveString=[instance[@"00081190"][@"Value"] firstObject];
-          if (RetrieveString && RetrieveString.length)
-          {
-             NSURL *RetrieveURL=[NSURL URLWithString:RetrieveString];
+         if (RetrieveString && RetrieveString.length)
+         {
+            
+            /*
+             transform
+             http://serviciosridi.asse.uy/dcm4chee-arc/wado/DCM4CHEE/studies/2.16.858.2.10001442.67430.20220906075913.5029238.8/series/2.16.858.2.10001442.67430.20220906075913.5029238.8.1/instances/2.16.858.2.10001442.67430.20220906075913.5029238.8.1.1
+             into "https://serviciosridi.asse.uy/dcm4chee-arc/wado/DCM4CHEE?requestType=WADO&studyUID=2.16.858.2.10001442.67430.20220906134444.5048465.8&seriesUID=2.16.858.2.10001442.67430.20220906134444.5048465.8.1&objectUID=2.16.858.2.10001442.67430.20220906134444.5048465.8.1.1&contentType=text/xml"
+             */
+            NSArray *a=[RetrieveString componentsSeparatedByString:@"/"];
+
+            NSString *wadoString;
+            if ([args[2] hasPrefix:@"https"])
+                wadoString=
+                [NSString stringWithFormat:
+                 @"https://%@/%@/%@/%@?requestType=WADO&studyUID=%@&seriesUID=%@&objectUID=%@&contentType=text/xml",
+                 a[2],
+                 a[3],
+                 a[4],
+                 a[5],
+                 a[7],
+                 a[9],
+                 a[11]
+                 ];
+            else //http
+                wadoString=
+                [NSString stringWithFormat:
+                 @"http://%@/%@/%@/%@?requestType=WADO&studyUID=%@&seriesUID=%@&objectUID=%@&contentType=text/xml",
+                 a[2],
+                 a[3],
+                 a[4],
+                 a[5],
+                 a[7],
+                 a[9],
+                 a[11]
+                 ];
+
+             NSURL *RetrieveURL=[NSURL URLWithString:wadoString];
              if (!RetrieveURL)
              {
                 NSLog(@"ERROR could not create URL from %@",RetrieveString);
                 continue;
              }
-             NSData *downloaded=[NSData dataWithContentsOfURL:RetrieveURL
-                                                      options:NSDataReadingUncached
-                                                        error:&localError];
+             NSData *downloaded=[NSData dataWithContentsOfURL:RetrieveURL options:NSDataReadingUncached error:&localError];
              if (!downloaded)
              {
                 NSLog(@"ERROR response to %@: %@",RetrieveString,localError.description);
                 continue;
              }
-
-                                  
-             if (downloaded.length == 0)
+            NSUInteger dl=downloaded.length;
+             if ( dl == 0)
              {
                  NSLog(@"empty response to %@",RetrieveString);
                  continue;
              }
-              
-              //unzip
-              ZZArchive *archive = [ZZArchive archiveWithData:downloaded];
-              ZZArchiveEntry *firstEntry = archive.entries[0];
-              NSData *unzipped = [firstEntry newDataWithError:&localError];
-              if (localError!=nil)
-              {
-                  NSLog(@"could NOT unzip response to %@: %@",RetrieveString,downloaded.description);
-                  continue;
-              }
-              
-              //get CDA: 00420011
-              NSRange range00420011=[unzipped rangeOfData:[NSData dataWithBytes:(void*)&tag00420011 length:8]
-                                                  options:0
-                                                    range:NSMakeRange(0,[unzipped length])];
-              if (range00420011.location==NSNotFound)
-              {
-                  NSLog(@"response to %@ NO contiene attr 00420011",RetrieveString);
-                  continue;
-              }
-              //get CDA: 00420011.length
-              uint32 capsuleLength=0x00000000;
-              [unzipped getBytes:&capsuleLength range:NSMakeRange(range00420011.location+8,4)];
-              
-              unsigned char padded=0xFF;
-              [unzipped getBytes:&padded range:NSMakeRange(range00420011.location+12+capsuleLength-1,1)];
-              NSData *encapsulatedData=[[NSData alloc]initWithData:[unzipped subdataWithRange:NSMakeRange(range00420011.location+12,capsuleLength-(padded==0))]];
-              
-              if (!encapsulatedData)
-              {
-                  NSLog(@"attr 00420011 empty: %@",RetrieveString);
-                  continue;
-              }
-              
-              NSXMLDocument *xmlDocument = [[NSXMLDocument alloc]initWithData:encapsulatedData options:0 error:&localError];
-              if (!xmlDocument)
-              {
+            NSXMLDocument *xmlDocument = nil;
+            uint16 last2Bytes=0;
+            [downloaded getBytes:&last2Bytes range:NSMakeRange(dl-2, 2)];
+            if (last2Bytes==0x000A)
+                xmlDocument = [[NSXMLDocument alloc]initWithData:[downloaded subdataWithRange:NSMakeRange(0,dl-2)] options:0 error:&localError];
+            else xmlDocument = [[NSXMLDocument alloc]initWithData:downloaded options:0 error:&localError];
+            if (!xmlDocument)
+            {
                   LOG_WARNING(@"00420011 NOT xml %@\r%@",RetrieveString,localError.description);
                   continue;
-              }
+            }
 #pragma mark write cda.xml
-              NSString *cdapath=[EUIDpath stringByAppendingPathComponent:@"cda.xml"];
-              [[xmlDocument XMLData] writeToFile:cdapath atomically:NO];
+            NSString *cdapath=[EUIDpath stringByAppendingPathComponent:@"cda.xml"];
+            [[xmlDocument XMLData] writeToFile:cdapath atomically:NO];
               
 #pragma mark write wl.json
-              //transform CDA 2 WorkItem (wl) json contextualkey-values
-              id wl = [xmlDocument objectByApplyingXSLT:xslt1Data
-                                                       arguments:nil
-                                                           error:&localError];
-              if (!wl)
-              {
+            //transform CDA 2 WorkItem (wl) json contextualkey-values
+            id wl = [xmlDocument objectByApplyingXSLT:xslt1Data arguments:nil error:&localError];
+            if (!wl)
+            {
                   NSLog(@"could not transform %@ to wl.json\r%@",cdapath,[localError description]);
                   continue;
-              }
+            }
               
-              if (![wl isKindOfClass:[NSData class]])
-              {
+            if (![wl isKindOfClass:[NSData class]])
+            {
                   NSLog(@"xslt1 on %@ did not output data file",cdapath);
                   continue;
-              }
+            }
 
-              NSString *wlpath=[EUIDpath stringByAppendingPathComponent:@"wl.json"];
-              [wl writeToFile:wlpath atomically:NO];
+            NSString *wlpath=[EUIDpath stringByAppendingPathComponent:@"wl.json"];
+            [wl writeToFile:wlpath atomically:NO];
 
 #pragma mark serialize wl json data to dicom
-              NSDictionary *json=[NSJSONSerialization JSONObjectWithData:wl options:0 error:&localError];
-              if (!json)
-              {
+            NSDictionary *json=[NSJSONSerialization JSONObjectWithData:wl options:0 error:&localError];
+            if (!json)
+            {
                   LOG_WARNING(@"ERROR reading %@: %@",wlpath,[localError description]);
                   continue;
-              }
+            }
 
-             NSMutableData *outputData=[NSMutableData dataWithLength:128];
-             [outputData appendBytes:&DICM length:4];
+            NSMutableData *outputData=[NSMutableData dataWithLength:128];
+            [outputData appendBytes:&DICM length:4];
  //fileMetadata
-             NSMutableData *outputFileMetadata=[NSMutableData data];
-             if (dict2D(@"",json[@"metadata"],outputFileMetadata,0,@{})==failure)
-             {
+            NSMutableData *outputFileMetadata=[NSMutableData data];
+            if (dict2D(@"",json[@"metadata"],outputFileMetadata,0,@{})==failure)
+            {
                 NSLog(@"could not serialize group 0002");
                 continue;
-             }
-             [outputData appendBytes:&_0002000_tag_vr length:8];
-             UInt32 fileMetadataLength=(UInt32)outputFileMetadata.length+14;
-             [outputData appendBytes:&fileMetadataLength length:4];
-             [outputData appendBytes:&_0002001_tag_vr length:8];
-             [outputData appendBytes:&_0002001_length length:4];
-             [outputData appendBytes:&_0002001_value length:2];
-             [outputData appendData:outputFileMetadata];
+            }
+            [outputData appendBytes:&_0002000_tag_vr length:8];
+            UInt32 fileMetadataLength=(UInt32)outputFileMetadata.length+14;
+            [outputData appendBytes:&fileMetadataLength length:4];
+            [outputData appendBytes:&_0002001_tag_vr length:8];
+            [outputData appendBytes:&_0002001_length length:4];
+            [outputData appendBytes:&_0002001_value length:2];
+            [outputData appendData:outputFileMetadata];
  // dataset
-             if (dict2D(@"",json[@"dataset"],outputData,0,@{})==failure)
-             {
+            if (dict2D(@"",json[@"dataset"],outputData,0,@{})==failure)
+            {
                 NSLog(@"could not serialize dataset");
                 continue;
-             }
-             [outputData writeToFile:[NSString stringWithFormat:@"%@/%@.wl",aetscppath,EUID] atomically:NO];
-          }//end retrieveString url prepared
+            }
+            [outputData writeToFile:[NSString stringWithFormat:@"%@/%@.wl",aetscppath,EUID] atomically:NO];
+         }//end retrieveString
      }
   }
    return 0;
